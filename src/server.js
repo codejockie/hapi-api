4096c56ce8603f2afdc2696eb4356f3d71b6e15a
John Kennedy (codejockie) <devjckennedy@gmail.com>
2018-07-04 23:12:13 +0100

    test other return types of handler

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 
 const Hapi = require('hapi')
 
+const { Article } = require('./models')
+
 const server = Hapi.server({
   host: 'localhost',
   port: 3000
@@ -10,7 +12,13 @@ server.route({
   method: 'GET',
   path: '/',
   handler: () => {
-    return [{ so: 'hapi!' }]
+    // return [{ so: 'hapi!' }]
+    // return 123
+    // return `HTML rules!`
+    // return null
+    // return new Error('Boom')
+    // return Promise.resolve({ whoa: true })
+    return require('fs').createReadStream('index.html')
   }
 })
 
